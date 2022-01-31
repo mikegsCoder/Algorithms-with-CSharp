cde7c05ff5a88dc34697834b79358a655dc04c9f
mikegsCoder <mikegsid@gmail.com>
2022-01-31 18:21:26 +0200

    Add new problem solutions in Recursion and Combinatorial problems - Exercise

---
+++ 01. Algorithms-Fundamentals/03. Recursion-And-Combinatorial-Problems-Exercise/05.SchoolTeams/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SchoolTeams
+{
+    internal class Program
+    {
+        private static string[] girls;
+        private static string[] boys;
+
+        private static readonly string[] girlsCombinations = new string[3];
+        private static readonly string[] boysCombinations = new string[2];
+
+        static void Main(string[] args)
+        {
+            girls = Console.ReadLine().Split(", ").ToArray();
+            boys = Console.ReadLine().Split(", ").ToArray();
+
+            GirlsWithBoysCombinations();
+        }
+
+
+        static void BoysCombinations(int index = 0, int start = 0, string girlsAsString = "")
+        {
+            if (index >= boysCombinations.Length)
+            {
+                var boysAsString = string.Join(", ", boysCombinations);
+
+                Console.WriteLine(girlsAsString + ", " + boysAsString);
+
+                return;
+            }
+
+
+            for (int i = start; i < boys.Length; i++)
+            {
+                boysCombinations[index] = boys[i];
+                BoysCombinations(index + 1, i + 1, girlsAsString);
+            }
+        }
+
+        static void GirlsWithBoysCombinations(int index = 0, int start = 0)
+        {
+            if (index >= girlsCombinations.Length)
+            {
+                var girlsAsString = string.Join(", ", girlsCombinations);
+                BoysCombinations(girlsAsString: girlsAsString);
+
+                return;
+            }
+
+
+            for (int i = start; i < girls.Length; i++)
+            {
+                girlsCombinations[index] = girls[i];
+                GirlsWithBoysCombinations(index + 1, i + 1);
+            }
+        }
+    }
+}
