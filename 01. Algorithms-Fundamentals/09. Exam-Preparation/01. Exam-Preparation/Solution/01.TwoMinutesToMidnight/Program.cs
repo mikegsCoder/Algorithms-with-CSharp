f7f02ee6dcd33cdfa29d99e342251abd1c7f8733
mikegsCoder <mikegsid@gmail.com>
2022-02-06 12:10:59 +0200

    Add new problem solutions in Exam Preparation

---
+++ 01. Algorithms-Fundamentals/09. Exam-Preparation/01. Exam-Preparation/Solution/01.TwoMinutesToMidnight/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.TwoMinutesToMidnight
+{
+    class Program
+    {
+        private static Dictionary<string, long> cache = new Dictionary<string, long>();
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+
+            var result = GetBinom(n, k);
+
+            Console.WriteLine(result);
+        }
+
+        private static long GetBinom(int row, int col)
+        {
+            var id = $"{row} {col}";
+
+            if (cache.ContainsKey(id))
+            {
+                return cache[id];
+            }
+
+            if (col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            var result = GetBinom(row - 1, col) + GetBinom(row - 1, col - 1);
+            cache[id] = result;
+
+            return result;
+        }
+    }
+}
---
+++ 01. Algorithms-Fundamentals/09. Exam-Preparation/01. Exam-Preparation/Solution/03.TheStoryTelling/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.TheStoryTelling
+{
+    class Program
+    {
+        private static Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+        private static Dictionary<string, int> dependencies = new Dictionary<string, int>();
+        private static List<string> sorted = new List<string>();
+
+        static void Main(string[] args)
+        {
+            ReadGraph();
+
+            ExtractDependencies();
+
+            TopologicalSorting();
+
+            Console.WriteLine(string.Join(" ", sorted));
+        }
+
+        private static void TopologicalSorting()
+        {
+            while (dependencies.Count > 0)
+            {
+                var nodeToRemove = dependencies.LastOrDefault(x => x.Value == 0);
+
+                if (nodeToRemove.Key == null)
+                {
+                    break;
+                }
+
+                var node = nodeToRemove.Key;
+                var children = graph[node];
+
+                sorted.Add(node);
+
+                foreach (var child in children)
+                {
+                    dependencies[child] -= 1;
+                }
+
+                dependencies.Remove(nodeToRemove.Key);
+            }
+        }
+
+        private static void ExtractDependencies()
+        {
+            foreach (var kvp in graph)
+            {
+                var node = kvp.Key;
+                var children = kvp.Value;
+
+                if (!dependencies.ContainsKey(node))
+                {
+                    dependencies.Add(node, 0);
+                }
+
+                foreach (var child in children)
+                {
+                    if (!dependencies.ContainsKey(child))
+                    {
+                        dependencies.Add(child, 1);
+                    }
+                    else
+                    {
+                        dependencies[child] += 1;
+                    }
+                }
+            }
+        }
+
+        private static void ReadGraph()
+        {
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                var elements = input.Split("->", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                var node = elements[0].Trim();
+                var children = elements.Length == 1
+                    ? new string[0]
+                    : elements[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                graph.Add(node, new List<string>(children));
+            }
+        }
+    }
+}
